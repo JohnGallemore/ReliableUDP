3ac814ddfe9cc6c7a935ec2404da3bbde413b865
JohnGallemore <gallemorejohn5@gmail.com>
2025-11-06 23:49:45 -0600

    Basic Setup
    I just did the outlines of all of the functions required for the send and receive for this project, none of them have been implemented yet.

--- RReceiveUDP.go
+++ RReceiveUDP.go
@@ -1,1 +1,37 @@
 package main
+
+func getFilenameR() string {
+	return ""
+}
+
+func getLocalPortR() int {
+	return 0
+}
+
+func getModeR() int {
+	return 0
+}
+
+func getModeParameterR() int64 {
+	return 0
+}
+
+func receiveFile() bool {
+	return true
+}
+
+func setFilenameR(fName string) {
+
+}
+
+func setLocalPortR(port int) bool {
+	return true
+}
+
+func setModeR(mode int) bool {
+	return true
+}
+
+func setModeParameterR(n int64) bool {
+	return true
+}
